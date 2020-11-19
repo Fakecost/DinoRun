f4abfe6cfabb6492973fa768db1d7a51b5b2912b
PHUPAKORN LUEHAN <42826315+Fakecost@users.noreply.github.com>
2020-11-19 22:46:26 +0700

    Add enemy

---
+++ DinoRun/Assets/Script/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D m_rigidbody2D;
+    public float _speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rigidbody2D.velocity = Vector2.left * _speed;
+    }
+}
